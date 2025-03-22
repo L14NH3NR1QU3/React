d8c758d2f3373f166d3f22dc1d343c2af85e640c
Lian <lianhenrique1902@gmail.com>
2025-03-22 02:15:55 -0300

    update teste

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <div className='planoFundo'></div>
-      <div className='nome'>
-        <h1 className='nome'>Lian Henrique Amarante Fernandes Silva</h1>
-      </div>
-      <div className='texto'>
-        <h2>Sobre mim</h2>
-      </div>
+      <Inicio />
     </>
   );
 }
---
+++ src/components/Inicio.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Inicio = () => {
+  return (
+    <div>
+        <div className='planoFundo'></div>
+        <div className='nome'>
+            <h1 className='nome'>Lian Henrique Amarante Fernandes Silva</h1>
+        </div>
+        <div className='texto'>
+            <h2>Sobre mim</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Inicio
